cf7ca6a94a0f5a149dcebfd34b55e88a2451de03
Roy Yang <roycyang@gmail.com>
2012-04-16 08:53:20 -0400

    archiving old files

---
+++ _archive/BrowserPanel.js
@@ -0,0 +1,72 @@
+Ext.define('Etsy.view.BrowserPanel', {
+    extend: 'Ext.Panel',
+
+    config: {
+        title: 'Categories',
+        iconCls: 'spaces2',
+        id: 'browserPanel',
+        layout: 'card',
+        items: [
+        
+            {
+                xtype: 'toolbar',
+                docked: 'top',
+                id: 'browserToolbar',
+                title: 'Categories',
+                items: [{
+                    xtype: 'button',
+                    action: 'showNav',
+                    iconCls: 'list',
+    				iconMask: true,
+                },
+                    // {
+                    //     xtype: 'button',
+                    //     ui: 'back',
+                    //     text: 'Back',
+                    //     id: 'browserBackButton',
+                    //     hidden: true
+                    // },
+                    {
+                        xtype: 'spacer'
+                    },
+                             //                    {
+                            //                         xtype: 'button',
+                            //                         id: 'browserFullCarouselButton',
+                            // iconCls: 'expand',
+                            // iconMask: true,
+                            // hidden: true
+                            //                     },
+                    // {
+                    //     xtype: 'button',
+                    //     iconCls: 'search',
+                    //     id: 'browserSearchButton',
+                    //     iconMask: true
+                    // },
+					{
+                        xtype: 'button',
+                        action: 'showSearch',
+                        iconCls: 'search',
+        				iconMask: true,
+                    },
+                ]
+            },
+            
+            {
+
+            xtype: 'dataview',
+            id: 'categoryList',
+            cls: 'products',
+            store: 'Categories',
+                itemTpl: [
+                '<div class="product category"><div class="category-name">{short_name}</div></div>'
+                ]
+            },
+            {
+                id: 'browserCarousel',
+                xtype: 'listingsCarousel',
+                flex: 2
+            }
+        ]
+    }
+
+});

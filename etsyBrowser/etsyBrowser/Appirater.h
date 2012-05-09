84e8b2f5a8a2643f0ca85cfde312fb4635e79085
Roy Yang <roycyang@gmail.com>
2012-05-08 20:52:05 -0400

    tweaking app rater

--- etsyBrowser/etsyBrowser/Appirater.h
+++ etsyBrowser/etsyBrowser/Appirater.h
@@ -99,7 +99,7 @@ extern NSString *const kAppiraterReminderRequestDate;
  Users need to 'use' the same version of the app this many times before
  before they will be prompted to rate it.
  */
-#define APPIRATER_USES_UNTIL_PROMPT		10		// integer
+#define APPIRATER_USES_UNTIL_PROMPT		5		// integer
 
 /*
  A significant event can be anything you want to be in your app. In a
